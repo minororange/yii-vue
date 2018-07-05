2ab405904b13b76e62645d09766cc79fd1791eca
yangchenzhuo <yangchenzhuo@elegomall.com>
2018-07-05 18:54:02 +0800

    修改.env APP_URL 将vue代理域名改为 env 中的 APP_URL

--- vue/config/index.js
+++ vue/config/index.js
@@ -13,7 +13,7 @@ module.exports = {
         assetsPublicPath: '/',
         proxyTable: {
             '/': {
-                target: 'http://yii.vue.com/', // 接口的域名
+                target: env.parsed.APP_URL, // 接口的域名
                 // secure: false,  // 如果是https接口，需要配置这个参数
                 changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
                 pathRewrite: {
